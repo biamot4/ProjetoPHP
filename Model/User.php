92a1f76bd3f3f0df69cbd7e3a36ef8123a2bd8b1
bia.mot4 <beatrizmotasesi@gmail.com>
2025-07-08 12:42:42 -0300

    feat: Estrutura inicial do projeto

---
+++ Model/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Model;
+
+use Model \ Connection;
+
+use POD;
+use PDOException;
+
+
+class User{
+
+    private $db;
+
+    /**
+     * MÉTODO QUE IRÁ SER EXECUTADO TODA VEZ QUE FOR CRIADO UM OBJETO DA CLASSE -> USER
+     */
+
+    public function __construct() {
+        $this->db = Connection::getInstance();
+    }
+    
+    //FUNÇÃO DE CRIAR USUÁRIO
+    public function registerUser($user_fullname, $email, $password){
+        try{
+            //INSERÇÃO DE DADOS NA LINGUAGEM SQL
+            $sql = 'INSERT INTO  user (user_fullname, email, password, created_at) VALUES (:user_fullname, :email, :password, NOW())';
+
+            // PREPARAR O BANCO DE DADOS PARA RECEBER O COMANDO ACIMA
+
+            $stmt = $this->db->prepare($sql);
+
+            //REFERENCIAR OS DADOS PASSADOS PELO COMANDO SQL COM OS PARÂMETROS DA FUNÇÃO
+            $stmt -> bindParam(":user_fullname" , var: $user_fullname, PDO::PARAM_STR);
+            
+            // EXECUTAR TUDO
+        }
+        catch(PDOException $error){}
+    }
+
+}
+
+?>
